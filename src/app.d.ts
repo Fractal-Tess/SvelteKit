a56330bb558a290b0da1001ce2d798533bd2269d
Fractal-Tess <vgfractal@gmail.com>
2022-09-12 19:39:26 +0300

    added tailwind and theme changing button

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,12 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-  // interface Locals {}
-  // interface PageData {}
+  interface Locals {
+    theme: import('./types').Theme | null;
+  }
+  interface PageData {
+    theme: import('./types').Theme | null;
+  }
   // interface PageError {}
   // interface Platform {}
 }

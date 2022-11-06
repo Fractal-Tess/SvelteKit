16eed74fc1c1714e78700de76c7a7e915f240171
Fractal-Tess <vgfractal@gmail.com>
2022-11-06 20:09:20 +0200

    added link type

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,14 @@
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
 export type Theme = 'dark' | 'light';
+
+export type NavLink = {
+  target: {
+    href: string;
+    newTab?: boolean;
+  };
+  content: {
+    text?: string;
+    icon?: IconDefinition;
+  };
+};
